d4375ac27c09ff4a446dd5400754ae21c3c7cd0b
Shriram <alekhshriram@gmail.com>
2022-12-12 10:18:03 -0600

    readme

---
+++ readme.md
@@ -0,0 +1,8 @@
+#### Rosalind problems
+
+Each problem is implemented in 
+1. Python
+2. C++
+3. Rust
+
+This project is going to take a long time
--- stronghold_cpp/dna.cpp
+++ stronghold_cpp/dna.cpp
@@ -1,8 +1,11 @@
 //!/usr/bin/g++
 #include <iostream>
+#include <fstream> //file i/o
 using namespace std;
 
 int main(){
 	cout<<"hello world";
+	fstream x;
+	x.open("data/rosalind_dna.txt",ios::in);
 	return 0;
 }
